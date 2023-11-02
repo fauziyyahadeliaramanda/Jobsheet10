b51ea3688ed14ec63cc500f9038d48154b84e3d1
faujiiiahhh <framandaadelia@gmail.com>
2023-11-02 10:09:20 +0700

    Modifikasi 3

--- BioskopWithScanner10.java
+++ BioskopWithScanner10.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class BioskopWithScanner10 {
     public static void main (String[] args){
-    Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         int baris, kolom;
         String nama, next;
         String[][] penonton = new String[4][2];
@@ -15,51 +15,53 @@ public class BioskopWithScanner10 {
 
             int menu = scan.nextInt();
 
-            switch (menu) {
-                case 1:
-                    System.out.print("Masukkan nama : ");
-                    nama = scan.next();
+        switch (menu) {
+            case 1:
+                System.out.print("Masukkan nama : ");
+                nama = scan.next();
+                do {
                     System.out.print("Masukkan baris : ");
                     baris = scan.nextInt();
                     System.out.print("Masukkan kolom : ");
                     kolom = scan.nextInt();
                     scan.nextLine();
-
-                    if (baris >= 1 && baris <= 4 && kolom <= 1 && kolom <=2) {
-            
+                    if (isValidSeat(baris, kolom, penonton)){
                     penonton[baris - 1][kolom - 1] = nama;
                     System.out.println("Data penonton telah dimasukkan.");
-                        }else {
-                    System.out.println("Baris atau kolom tidak valid.");
-                        }
-                     break;
-                case 2 :
-                    System.out.println("Daftar Penonton : ");
-                        for (int i = 0; i < 4; i++){
-                             for(int j = 0; j < 2; j++){
-                                if (penonton[i][j] != null){
-                                     System.out.println("Baris " + (i+1) + ",  Kolom " + (j+1) + ": " + penonton[i][j]);
-                        }
-                     }
-                 }
-                break;
+        
+                    break;
+                }else {
+                    System.out.println("Kursi telah terisi. Silahkan pilih kursi lain.");
+             }
+            }while (true);
+            break;
 
-                case 3 :
-                    System.out.println("Terima kasih, program telah selesai.");
-                    System.exit(0);
-                     default :
-                    System.out.println("Menu tidak valid. Silahkan pilih 1, 2, atau 3.");
-                break;
-                    }
+            case 2 :
+                System.out.println("Daftar Penonton : ");
+                for (int i = 0; i < 4; i++){
+                    for(int j = 0; j < 2; j++){
+                         if (penonton[i][j] != null){
+                System.out.println("Baris " + (i+1) + ",Kolom " + (j+1) + ": " + penonton[i][j]);
+                 }
+                }
+            }
+            break;
+
+             case 3 :
+                System.out.println("Terima kasih, program telah selesai.");
+                System.exit(0);
+                default :
+                     System.out.println("Menu tidak valid. Silahkan pilih 1, 2, atau 3.");
+            break;
                 }
              }
-               static boolean isValidSeat(int baris, int kolom, String[][] penonton){
-                    if (baris >= 1 && baris <= 4 && kolom >= 1 && kolom <=2){
-                        if (penonton[baris - 1][kolom - 1] == null){
-                            return true;
+        }
+                 static boolean isValidSeat(int baris, int kolom, String[][] penonton){
+                        if (baris >= 1 && baris <= 4 && kolom >= 1 && kolom <=2){
+                            if (penonton[baris - 1][kolom - 1] == null){
+                                return true;
                         }
                     }
                     return false;
                 }
-
-            }
+        }
