9ad0cd2d6c0e7ba4221b37a981f5d9a47130f995
faujiiiahhh <framandaadelia@gmail.com>
2023-11-02 08:47:48 +0700

    Modifikasi 3

--- Bioskop10.java
+++ Bioskop10.java
@@ -21,7 +21,14 @@ public class Bioskop10 {
             for (String[] barisPenonton : penonton){
                 System.out.println("Panjang baris : " + barisPenonton.length);
             }
-        }
+            System.out.println("Penonton pada baris ke-3: ");
+             for(String i : penonton[2]){
+                System.out.println(i);
+             }
+             }
+                }
+            
         
-    }   
+        
+    
 
